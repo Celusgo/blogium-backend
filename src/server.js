ca8f3c2a4c813ad7bfb9644177d329edeb3ad36a
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-06-10 10:56:13 -0300

    Sending each comments' array to it's post

--- src/server.js
+++ src/server.js
@@ -7,13 +7,15 @@ app.use(cors());
 
 const posts = [];
 
+const comments = [];
+
 app.get("/posts", (req, res) => {
     res.send(posts);
 })
 
 app.get("/posts/:id", (req, res) => {
     const postId = parseInt(req.params.id);
-    const thisPost = posts.filter(i => i.id === postId)
+    const thisPost = posts.filter(each => each.id === postId)
     res.send(thisPost[0]);
 })
 
@@ -23,6 +25,20 @@ app.post("/posts", (req,res) => {
     posts.push(incomingPost);
     res.send(incomingPost);
 });
+
+app.post("/posts/:id/comments", (req, res) => {
+    const postId = req.params.id;
+    let incomingNewComment = req.body;
+    incomingNewComment = {...incomingNewComment, id: comments.length + 1, postId: postId};
+    comments.push(incomingNewComment);
+    res.send(incomingNewComment);
+})
+
+app.get("/posts/:id/comments", (req, res) => {
+    const postId = req.params.id;
+    const thisPostComments = comments.filter(each => each.postId == postId);
+    res.send(thisPostComments)
+})
 
 
 app.listen(4001, ()=>{
